7c6286afadecae865c449aa91e6da6d0f17cfd56
sumeet.rana <sumeetsinh28@gmail.com>
2023-11-29 12:19:35 +0530

    feat: tweet crud

--- src/routes/tweetRoute.ts
+++ src/routes/tweetRoute.ts
@@ -1,28 +1,59 @@
-import {Router} from 'express';
+import { PrismaClient } from '@prisma/client';
+import { Router } from 'express';
 
 const router = Router();
+const prisma = new PrismaClient();
 
-router.post("/", (req, res) => {
-  res.status(501).json({error: 'Not implemented'})
+router.post("/", async (req, res) => {
+  const { content, userId } = req.body;
+  try {
+    const result = await prisma.tweet.create({
+      data: {
+        content,
+        userId //TODO: manage based on the auth user
+      }
+    })
+    res.status(201).json({result})
+  } catch(error) {
+    res.status(400).json({error: "UserId does not exist"})
+  }
 })
 
-router.get("/", (req, res) => {
-  res.status(501).json({error: 'Not implemented'})
+router.get("/", async (req, res) => {
+  const allTweets = await prisma.tweet.findMany();
+  res.status(200).json({tweets: allTweets})
 })
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
-  res.status(501).json({error: 'Not implemented'})
+  const tweet = await prisma.tweet.findUnique({
+    where: {id: Number(id)}
+  })
+  res.status(200).json({tweet})
 })
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   const { id } = req.params;
-  res.status(501).json({error: 'Not implemented'})
+  const {content} = req.body;
+
+  try {
+    const result = await prisma.tweet.update({
+      where: {id: Number(id)},
+      data: {content}
+    })
+    res.status(204).json({result})
+  } catch(e) {
+    res.status(400).json({error: "Tweet doesn't exist'"})
+  }
+
 })
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   const { id } = req.params;
-  res.status(501).json({error: 'Not implemented'})
+  await prisma.tweet.delete({
+    where: {id: Number(id)}
+  })
+  res.status(200).json({message: 'Tweet deleted'})
 })
 
 export default router;
